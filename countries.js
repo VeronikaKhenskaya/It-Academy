5bd5d75745a1c1ce8b24ec7a9c70d0e1d1348bc3
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-04-17 18:57:35 +0200

    changes

--- countries.js
+++ countries.js
@@ -1,32 +1,40 @@
 
 
-    var countrysH={};
+let countrysH = {};
 
-    function addCountry(countryName,capitalName) {
-        countrysH[countryName]=capitalName;
-    }
 
-    function deleteCountry(countryName) {
-        delete countrysH[countryName];
-    }
 
-    function getCountryInfo(countryName) {
-        if ( countryName in countrysH )
-            return 'страна: ' + countryName + ' столица: ' + countrysH[countryName] ;
-        else
-            return 'нет информации о стране ' + countryName + '!' ;
-    }
+function addCountry(countryName, capitalName) {
+let countryName = prompt("Введите название страны");
+let capitalName = prompt("Введите название столицы");
+    countrysH[countryName] = capitalName;
+}
+console.log(countrysH)
 
-    function listCountrys() {
-        var res="";
-        for ( var CN in countrysH )
-            res+='\n'+getCountryInfo(CN);
-        return res;
-    }
+function deleteCountry(countryName) {
+    let countryName = prompt("Введите название страны");
+    delete countrysH[countryName];
+}
 
-    addCountry('Германия','Берлин');
-    addCountry('Венгрия','Будапешт');
-    addCountry('Франция','Париж');
+function getCountryInfo(countryName) {
+    let countryName = prompt("Введите название страны");
+    if (countryName in countrysH)
+        return 'страна: ' + countryName + ' столица: ' + countrysH[countryName];
+    else
+        return 'нет информации о стране ' + countryName + '!';
+}
 
-    console.log( "список стран:" + listCountrys() );
+function listCountrys() {
+   
+    var res = "";
+    for (var CN in countrysH)
+        res += '\n' + getCountryInfo(CN);
+    return res;
+}
+
+addCountry('Германия', 'Берлин');
+addCountry('Венгрия', 'Будапешт');
+addCountry('Франция', 'Париж');
+
+console.log("список стран:" + listCountrys());
 
