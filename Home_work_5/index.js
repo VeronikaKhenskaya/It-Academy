a7c6e22b90561bd31582b4ce98be7e5fc47d962d
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-08-05 22:00:40 +0200

    style changes

--- Home_work_5/index.js
+++ Home_work_5/index.js
@@ -19,7 +19,15 @@ fetch('https://dummyjson.com/products')
       newCard.getElementsByClassName("price")[0].textContent = `Price: ${product.price}$`;
       newCard.getElementsByClassName("rating")[0].textContent = `Rating: ${product.rating}`;
     }
-  });
+  })
 
-
-
+window.addEventListener('resize', resizeScreen);
+function resizeScreen() {
+  for (let card of document.getElementsByClassName("card")) {
+    if (window.innerWidth <= 768) {
+      card.style.width = '90%';
+    } else {
+      card.style.width = '20%';
+    }
+  }
+}
