30e877c2a67879de864447b253fd5267935e63fd
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-08-05 20:31:28 +0200

    functions added

--- Home_work_5/index.js
+++ Home_work_5/index.js
@@ -1,6 +1,25 @@
+function createCard() {
+  let container = document.getElementById("main-container");
+  let parent = document.getElementsByClassName("card")[0];
+  let clone = parent.cloneNode(true);
+  container.appendChild(clone);
+  return clone;
+}
+
 fetch('https://dummyjson.com/products')
   .then(res => res.json())
   .then((data) => {
     console.log(data.products);
-    // Start here :)
+    const productsArray = data.products;
+    for (const product of productsArray) {
+      let newCard = createCard();
+      newCard.getElementsByClassName("card-img-top")[0].src = product.images[0];
+      newCard.getElementsByClassName("card-title")[0].textContent = product.title;
+      newCard.getElementsByClassName("card-text")[0].textContent = product.description;
+      newCard.getElementsByClassName("price")[0].textContent = `Price: ${product.price}`;
+      newCard.getElementsByClassName("rating")[0].textContent = `Rating: ${product.rating}`;
+    }
   });
+
+
+
