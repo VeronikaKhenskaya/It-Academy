372fc3227ad7f6e6b90fe1466ed184a80abc1124
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-08-07 14:47:26 +0200

    files created

---
+++ JS_Home_work_6/index.js
@@ -0,0 +1,33 @@
+function createCard() {
+  let container = document.getElementById("main-container");
+  let parent = document.getElementsByClassName("card")[0];
+  let clone = parent.cloneNode(true);
+  container.appendChild(clone);
+  return clone;
+}
+
+fetch('https://dummyjson.com/products')
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data.products);
+    const productsArray = data.products;
+    for (const product of productsArray) {
+      let newCard = createCard();
+      newCard.getElementsByClassName("card-img-top")[0].src = product.images[0];
+      newCard.getElementsByClassName("card-title")[0].textContent = product.title;
+      newCard.getElementsByClassName("card-text")[0].textContent = product.description;
+      newCard.getElementsByClassName("price")[0].textContent = `Price: ${product.price}$`;
+      newCard.getElementsByClassName("rating")[0].textContent = `Rating: ${product.rating}`;
+    }
+  })
+
+window.addEventListener('resize', resizeScreen);
+function resizeScreen() {
+  for (let card of document.getElementsByClassName("card")) {
+    if (window.innerWidth <= 768) {
+      card.style.width = '90%';
+    } else {
+      card.style.width = '20%';
+    }
+  }
+}
