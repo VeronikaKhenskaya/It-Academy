52384a68b39577df928b3fc086908f2b6987e205
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-08-16 18:09:14 +0200

    changes

--- JS_Home_work_6/index.js
+++ JS_Home_work_6/index.js
@@ -1,15 +1,16 @@
-function createCard() {
-  let container = document.getElementById("main-container");
-  let parent = document.getElementsByClassName("card")[0];
-  let clone = parent.cloneNode(true);
-  container.appendChild(clone);
-  return clone;
-}
-
 fetch('https://dummyjson.com/products')
   .then(res => res.json())
   .then((data) => {
-    console.log(data.products);
+    //console.log(data.products);
+
+    function createCard() {
+      let container = document.getElementById("main-container");
+      let parent = document.getElementsByClassName("card")[0];
+      let clone = parent.cloneNode(true);
+      container.appendChild(clone);
+      return clone;
+    }
+
     const productsArray = data.products;
     for (const product of productsArray) {
       let newCard = createCard();
@@ -19,44 +20,47 @@ fetch('https://dummyjson.com/products')
       newCard.getElementsByClassName("price")[0].textContent = `Price: ${product.price}$`;
       newCard.getElementsByClassName("rating")[0].textContent = `Rating: ${product.rating}`;
     }
+  })
 
 
+  window.addEventListener("click", function (event) {
+    if (event.target.hasAttribute("href")) {
+      const card = event.target.closest(".card-body");
+      let productInfo = {
+        title: card.querySelector(".card-title").innerText,
+        imgSrc: card.closest(".card").querySelector(".card-img-top").hasAttribute("src"),
+        price: card.querySelector(".price").innerText,
+      }
 
-    window.addEventListener("click", function (event) {
-      if (event.target.hasAttribute("href")) {
-
-        const card = event.target.closest(".card-body");
-        let productInfo = {
-          title: card.querySelector(".card-title").innerText,
-          imgSrc: card.closest(".card").querySelector(".card-img-top").hasAttribute("src"),
-          price: card.querySelector(".price").innerText,
-        }
-
-        let n = document.querySelector(".is-quantity").innerText;
-        n++;
-
-        const cartItemHtml = ` <div class="item-information">
+      const cartItemHtml = ` <div class="item-information">
         <div class="item-image">${productInfo.imgSrc}</div>
         <div class="item-title">${productInfo.title}</div>
         <div class="quantity-price">
           <div data-id="minus-quantity" data-id="minus">-</div>
           <div class="common-price-quantity">
-            <div class="is-quantity">${n}</div>
+            <div class="is-quantity">0</div>
             <div class="multiply">x</div>
             <div class="is-price">${productInfo.price}</div>
           </div>
           <div class="plus-quantity" data-id="plus">+</div>
         </div>
-        <a class="delete-btn">Remove from order</a>
       </div>`
 
-        const cartWrapper = document.querySelector(".main-container");
-        cartWrapper.insertAdjacentHTML("beforeend", cartItemHtml);
-
-      }
-    })
-
+      const cartWrapper = document.querySelector(".main-container");
+      const itemInCartInfo = cartWrapper.children[1];
+      console.log(cartWrapper.children)
+      let itemInCart = itemInCartInfo.children[1].innerText;
+      //let OrderBtn = `<a class="order-btn">Order</a>`
+      cartWrapper.insertAdjacentHTML("beforeend", cartItemHtml);
+      //shoppingCart.insertAdjacentHTML("beforeend", OrderBtn);
+      let productInCart = document.getElementsByClassName(".item-information");
+    }
   })
+
+
+
+
+
 
 window.addEventListener('resize', resizeScreen);
 function resizeScreen() {
@@ -74,7 +78,8 @@ let shoppingCart = document.getElementsByClassName("main-container")[0];
 let closeButton = document.getElementsByClassName("close-btn")[0];
 
 function openCart() {
-  shoppingCart.style.display = "block";
+  shoppingCart.style.display = "flex";
+  shoppingCart.style.display.justifyContent = "space-between";
   document.body.style.background = "#B4B4B4";
   document.body.style.position = "fixed";
 }
