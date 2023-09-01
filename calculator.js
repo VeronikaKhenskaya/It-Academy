a14ace38fc207e3cb36d2152b8253b27330bf29e
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-09-01 13:38:07 +0200

    calculator

---
+++ calculator.js
@@ -0,0 +1,43 @@
+class Calculator {
+    constructor() {
+      this.input = 0;//запоминает ввод нового числа//
+      this.currentOperation = null;
+      this.lastOperation = null;
+      this.result = 0; //текущий результат предыдущих операций//
+    }
+    
+    inputDigit(digit) {
+      if (this.currentOperation === null) {
+        this.input = this.input * 10 + digit;
+      } else {
+        this.lastOperation = this.currentOperation;
+        this.currentOperation = null;   
+        this.input = digit;
+      }
+    }
+    
+    inputOperation(operation) {
+      if(this.currentOperation === null) {
+        if(this.lastOperation !== null) {
+          this.performLastOperation();
+        } else {
+          this.result = this.input;
+        }
+        this.currentOperation = operation;
+      } else {
+        this.currentOperation = operation;
+      }
+    }
+    
+    performLastOperation() {
+      if(this.lastOperation === '+') {
+        this.result = this.result + this.input;
+      } else if(this.lastOperation === '-'){
+         this.result = this.result - this.input;
+      } else if(this.lastOperation === '/'){
+         this.result = this.result / this.input;
+      } else if(this.lastOperation === '*'){
+         this.result = this.result * this.input;
+      } 
+    }
+  }
