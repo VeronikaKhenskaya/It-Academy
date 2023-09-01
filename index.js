12f5b0376d59ae43fd914e0159f7ea29f2537ab6
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-09-01 13:38:18 +0200

    calculator

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
-class Calculator {
-    constructor() {
-      this.input = 0;//запоминает ввод нового числа//
-      this.currentOperation = null;
-      this.lastOperation = null;
-      this.result = 0; //текущий результат предыдущих операций//
-    }
-    
-    inputDigit(digit) {
-      if (this.currentOperation === null) {
-        this.input = this.input * 10 + digit;
-      } else {
-        this.lastOperation = this.currentOperation;
-        this.currentOperation = null;   
-        this.input = digit;
-      }
-      
-    }
-    
-    inputOperation(operation) {
-      if(this.currentOperation === null) {
-        if(this.lastOperation !== null) {
-          this.performLastOperation();
-        } else {
-          this.result = this.input;
-        }
-        this.currentOperation = operation;
-      } else {
-        this.currentOperation = operation;
-      }
-    }
-    
-    performLastOperation() {
-      if(this.lastOperation === '+') {
-        this.result = this.result + this.input;
-      } else if(this.lastOperation === '-'){
-         this.result = this.result - this.input;
-      } else if(this.lastOperation === '/'){
-         this.result = this.result / this.input;
-      } else if(this.lastOperation === '*'){
-         this.result = this.result * this.input;
-      }
-     
-    }
-  }
+calc = new Calculator();
+
+calc.inputDigit(1);
+calc.inputDigit(2);
+calc.inputOperation('+');
+calc.inputDigit(3);
+calc.inputDigit(4);
+calc.inputOperation('+');
+calc.inputDigit(5);
+calc.inputDigit(6);
+console.log(calc);
