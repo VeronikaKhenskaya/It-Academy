5ec62b751978cafd25eacb47307c69fab6ba0d5a
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-09-21 15:42:15 +0200

    variables added

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-let inputData = document.querySelector(".inputData").value;
-console.log(inputData);
+function inputSearch(){
+    let inputData = document.querySelector(".inputData").value;
+    console.log(inputData);
+}
 
-
-function enter() {
+function bindEnterKey() {
     document.querySelector(".inputData").addEventListener('keydown', function (enter) {
         if (enter.key === "Enter") {
             console.log(this.value);
         }
     });
 };
-enter();
+bindEnterKey();
 
 let todayWeatherIcon = document.querySelector("#todayIcon");
 let todayTemperature = document.querySelector(".todayTemperature").innerText;
-let location = document.querySelector(".location").innerText;
-let todaySunsetTime = document.querySelector(".time").innerText;
+let placeToSearch = document.querySelector(".location").innerText;
+let todaySunsetTime = document.querySelector(".sunset-time").innerText;
 
 let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let date = new Date();
 let day = weekday[date.getDay()];
 document.querySelector("#today-day").innerText = day;
 console.log(day);
+
+let humidityData = document.querySelector("#three").innerText;
+let sunsetTime = document.querySelector("#six").innerText;
+let sunriseTime = document.querySelector("#twelve").innerText;
+let uvIndex = document.querySelector("#nine").innerText;
+let monthlyRain = document.querySelector("#rainfall-data").innerText;
+let annualRain = document.querySelector("#rrainfall-changes").innerText;
