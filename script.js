9a81ea83768bf6464c13f94b2dd1cb981c3bf1df
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-09-21 14:41:58 +0200

    js updated

---
+++ script.js
@@ -0,0 +1,23 @@
+function search() {
+    let searchLocation = document.querySelector(".search").value;
+    console.log(searchLocation);
+}
+
+function enter() {
+    document.querySelector(".search").addEventListener('keydown', function(enter) {
+      if (enter.key === "Enter") {
+        console.log(this.value);
+      }
+    });
+  };
+  enter();
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const date = new Date();
+let day = weekday[date.getDay()];
+document.getElementById("today-day").innerHTML = day;
+
+  let todayWeatherIcon = document.querySelector("#todayIcon");
+  let todayTemperature = document.querySelector(".todayTemperature").innerText;
+  let location = document.querySelector(".location").innerText;
+  let todaySunsetTime = document.querySelector(".time").innerText;
