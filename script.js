8cadadd8c17d55af7a263b0e2099347e14f97ed6
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-10-06 15:24:05 +0300

    changes

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function findWeatherData(city) {
   console.log(data);
   document.querySelector(".todayTemperature").innerText = Math.round(data.list[0].main.temp) + "\xB0";
   document.querySelector("#three").innerText = data.list[0].main.humidity + '%';
-  document.querySelector("#max-temp-data").innerText = Math.round(data.list[0].main.temp_max) + " \xB0" + "C" ;
+  document.querySelector("#max-temp-data").innerText = Math.round(data.list[0].main.temp_max) + " \xB0" + "C";
   document.querySelector("#min-temp-data").innerText = Math.round(data.list[0].main.temp_min) + " \xB0" + "C";
   document.querySelector(".location").innerText = `${data.city.name}, ${data.city.country}`;
   document.querySelector("#weather-description").innerText = data.list[0].weather[0].description;
@@ -67,19 +67,12 @@ async function findWeatherData(city) {
   document.querySelector("#twelve").innerText = sunriseFormattedTime;
 }
 
-//let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//let date = new Date();
-//let day = weekday[date.getDay()];
-//document.querySelector("#today-day").innerText = day;
-//console.log(day);
-
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let d = new Date();
 console.log(d);
 let dayOfWeek = weekday[d.getDay()];
 console.log(dayOfWeek);
-//document.querySelector(".week-day").innerText = dayOfWeek;
 let todayDate = d.getDate();
 console.log(todayDate);
 let todayMonth = months[d.getMonth()];
@@ -92,26 +85,49 @@ let humidityData = document.querySelector("#three").innerText;
 let sunsetTime = document.querySelector("#six").innerText;
 let sunriseTime = document.querySelector("#twelve").innerText;
 
+/*for (let i = 0; i < 5; i++) {
+  let nextDay = new Date();
+  nextDay.setDate(nextDay.getDate() + i)
+  //console.log(nextDay)
+  const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let dayOfWeek = weekday[nextDay.getDay()];
+  //console.log(dayOfWeek)
+  let todayDate = nextDay.getDate() - 1;
+  //console.log(todayDate);
+  let todayMonth = months[nextDay.getMonth()];
+  //console.log(todayMonth);
+  //console.log(`${dayOfWeek}, ${todayMonth} ${todayDate}`)
+  let dayForecast = document.getElementsByClassName("day-forecast")[i];
+  //console.log(dayForecast);
+  let weekDayElement = dayForecast.getElementsByClassName("week-day")[0];
+  //console.log(weekDayElement)
+  weekDayElement.textContent = "`${dayOfWeek}, ${todayMonth} ${todayDate}`"
+}*/
 
-//const options = {
-//  enableHighAccuracy: true,
-// timeout: 5000,
-// maximumAge: 0,
-//};
-
-//function success(pos) {
-//const crd = pos.coords;
-//const lat = crd.latitude;
-//const lon = crd.longitude;
-//console.log("Your current position is:");
-// console.log(`Latitude : ${lat}`);
-// console.log(`Longitude: ${lon}`);
-//}
-//function error(err) {
-//console.warn(`ERROR(${err.code}): ${err.message}`);
-//}
-//z = navigator.geolocation.getCurrentPosition(success, error, options);
-//console.log(z);
+function setForecastDates() {
+  const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  for (let i = 0; i < 5; i++) {
+    let nextDay = new Date();
+    nextDay.setDate(nextDay.getDate() + i)
+    //console.log(nextDay)
+    let dayOfWeek = weekday[nextDay.getDay()];
+    //console.log(dayOfWeek)
+    let todayDate = nextDay.getDate();
+    //console.log(todayDate);
+    let todayMonth = months[nextDay.getMonth()];
+    //console.log(todayMonth);
+    //console.log(`${dayOfWeek}, ${todayMonth} ${todayDate}`)
+    let dayForecast = document.getElementsByClassName("day-forecast")[i];
+    //console.log(dayForecast);
+    let weekDayElement = dayForecast.firstChild;
+    //console.log(weekDayElement)
+    weekDayElement.textContent = `${dayOfWeek} ${todayMonth} ${todayDate}`;
+    //console.log(weekDayElement)
+  }
+}
+setForecastDates()
 
 /* function inputSearch() {
   let stored = localStorage.getItem('storageArray');
@@ -132,61 +148,44 @@ inputSearch(); */
 
 window.addEventListener('resize', resizeScreen);
 function resizeScreen() {
-    if (window.innerWidth <= 947) {
-      document.getElementsByTagName('header')[0].style.flexDirection = "column"
-      document.getElementsByTagName('header')[0].style.width = "100%";
-      document.querySelector(".main-table").style.width = "100%";
-      document.querySelector(".container").style.width = "100%";
-      document.getElementsByTagName('input')[0].style.width = '100%';
-      document.getElementsByTagName('h1')[0].style.width = '100%';
-      document.getElementsByTagName('h1')[0].style.textAlign = "center";
-      document.querySelector(".container").style.gridTemplateColumns = "100%"
-      document.querySelector(".container").style.gridTemplateRows = "1fr 1fr 1fr";
-      document.querySelector(".container").style.justifyContent = "center";
-      document.querySelector(".weather-parameters").style.gridColumn = "1";
-      document.querySelector(".weather-parameters").style.gridRow = "1";
-      document.querySelector(".max-min-temperature").style.gridColumn = "1";
-      document.querySelector(".max-min-temperature").style.gridRow = "2";
-      document.querySelector(".forecast-container").style.gridColumn = "1";
-      document.querySelector(".forecast-container").style.gridRow = "3";
-    } else {
-      document.getElementsByTagName('header')[0].style.flexDirection = "row"
-      document.getElementsByTagName('header')[0].style.width = "90%";
-      document.querySelector(".main-table").style.width = "90%";
-      document.querySelector(".container").style.width = "90%";
-      document.getElementsByTagName('input')[0].style.width = '80%';
-      document.getElementsByTagName('h1')[0].style.width = '';
-      document.getElementsByTagName('h1')[0].style.textAlign = "";
-      document.querySelector(".container").style.gridTemplateColumns = "50% 50%"
-      document.querySelector(".container").style.gridTemplateRows = "100px 100px";
-      document.querySelector(".container").style.justifyContent = "";
-      document.querySelector(".weather-parameters").style.gridColumn = "1";
-      document.querySelector(".weather-parameters").style.gridRow = "1";
-      document.querySelector(".max-min-temperature").style.gridColumn = "1";
-      document.querySelector(".max-min-temperature").style.gridRow = "2";
-      document.querySelector(".forecast-container").style.gridColumn = "2";
-      document.querySelector(".forecast-container").style.gridRow = "1/3";
-    }
-
-    for(let i=0; i < 5; i++) {
-      let nextDay = new Date();
-      nextDay.setDate(nextDay.getDate() + i)
-      //console.log(nextDay)
-      const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      let dayOfWeek = weekday[nextDay.getDay()];
-      //console.log(dayOfWeek)
-      let todayDate = nextDay.getDate() - 1;
-     //console.log(todayDate);
-     let todayMonth = months[nextDay.getMonth()];
-     //console.log(todayMonth);
-        console.log(`${dayOfWeek}, ${todayMonth} ${todayDate}`)
-       let dayForecast =  document.getElementsByClassName("day-forecast")[i];
-    console.log(dayForecast);
-    let weekDayElement = dayForecast.getElementsByClassName("week-day")[0];
-      weekDayElement.innerText = `${dayOfWeek}, ${todayMonth} ${todayDate}`
-    }
+  if (window.innerWidth <= 947) {
+    document.getElementsByTagName('header')[0].style.flexDirection = "column"
+    document.getElementsByTagName('header')[0].style.width = "100%";
+    document.querySelector(".main-table").style.width = "100%";
+    document.querySelector(".container").style.width = "100%";
+    document.getElementsByTagName('input')[0].style.width = '100%';
+    document.getElementsByTagName('h1')[0].style.width = '100%';
+    document.getElementsByTagName('h1')[0].style.textAlign = "center";
+    document.querySelector(".container").style.gridTemplateColumns = "100%"
+    document.querySelector(".container").style.gridTemplateRows = "1fr 1fr 1fr";
+    document.querySelector(".container").style.justifyContent = "center";
+    document.querySelector(".weather-parameters").style.gridColumn = "1";
+    document.querySelector(".weather-parameters").style.gridRow = "1";
+    document.querySelector(".max-min-temperature").style.gridColumn = "1";
+    document.querySelector(".max-min-temperature").style.gridRow = "2";
+    document.querySelector(".forecast-container").style.gridColumn = "1";
+    document.querySelector(".forecast-container").style.gridRow = "3";
+  } else {
+    document.getElementsByTagName('header')[0].style.flexDirection = "row"
+    document.getElementsByTagName('header')[0].style.width = "90%";
+    document.querySelector(".main-table").style.width = "90%";
+    document.querySelector(".container").style.width = "90%";
+    document.getElementsByTagName('input')[0].style.width = '80%';
+    document.getElementsByTagName('h1')[0].style.width = '';
+    document.getElementsByTagName('h1')[0].style.textAlign = "";
+    document.querySelector(".container").style.gridTemplateColumns = "50% 50%"
+    document.querySelector(".container").style.gridTemplateRows = "100px 100px";
+    document.querySelector(".container").style.justifyContent = "";
+    document.querySelector(".weather-parameters").style.gridColumn = "1";
+    document.querySelector(".weather-parameters").style.gridRow = "1";
+    document.querySelector(".max-min-temperature").style.gridColumn = "1";
+    document.querySelector(".max-min-temperature").style.gridRow = "2";
+    document.querySelector(".forecast-container").style.gridColumn = "2";
+    document.querySelector(".forecast-container").style.gridRow = "1/3";
   }
+
+
+}
 
 
 
