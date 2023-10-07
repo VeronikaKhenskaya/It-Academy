e394d01a4f2c42c7e250d2b41e29c791cf6dd625
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-10-08 02:32:35 +0300

    update

--- input-search.js
+++ input-search.js
@@ -8,83 +8,53 @@
         })
 
         function showLastSearches() {
-            let lastSearches = JSON.parse(localStorage.getItem('storageArray'));
             let inputDiv = document.querySelector(".inputSearchList");
+            let lastSearches = getLastStoredSearches();
+            if (lastSearches.length === 0) {
+                inputDiv.style.display = "none";
+            } else {
+                inputDiv.style.display = "block";
+            }
             let list = document.createElement('div');
             list.className = "list-container";
             inputDiv.replaceChildren(list);
             for (let i = 0; i < lastSearches.length; i++) {
-                let li = document.createElement('div');
-                li.className = "last-searches-container";
-                let lastinput = list.appendChild(li);
-                lastinput.textContent = `${lastSearches[i]}`;
-                //создание div с крестиком
-                let x = document.createElement('div');
-                x.className = "cross"
-                x.textContent = 'x'
-                li.appendChild(x);
-            };
-            inputDiv.style.display = "block";
-            changeColorOfSearchesList();
-
-            //сделать отдельной функцией
-            let arrayOfInputValues = Array.from(list.children);
-            for (let i = 0; i < arrayOfInputValues.length; i++) {
-                arrayOfInputValues[i].addEventListener('click', function () {
-                    document.querySelector(".inputData").value = event.target.textContent;
-                    
-                    list.style.display = "none";
+                let previousSearchContainer = list.appendChild(document.createElement('div'));
+                previousSearchContainer.className = "previous-search-container";
+                let searchText = previousSearchContainer.appendChild(document.createElement('div'));
+                searchText.className = "search-text";
+                searchText.textContent = `${lastSearches[i]}`;
+                searchText.addEventListener('click', function (event) {
+                    if (!event.target.classList.contains('delete-search')) {
+                        document.querySelector(".inputD ata").value = event.target.textContent;
+                        inputDiv.style.display = "none";
+                    }
                 })
-            }
-        }
-
-        function createCrossInDiv() {
-
-        }
-
-        function inputSearch() {
-            let list = document.querySelector(".inputSearchList");
-            list.style.display = "block";
-            let inputData = document.querySelector(".inputData").value;
-            //findWeatherData(inputData);
-            let stored = localStorage.getItem('storageArray');
-            let lastSearches;
-            if (stored === null) {
-                lastSearches = []
-            } else {
-                lastSearches = JSON.parse(stored);
-            }
-            if (lastSearches.length >= 10) {
-                lastSearches.shift();
-            }
-            //arr.push => arr.unshift
-            lastSearches.unshift(document.querySelector(".inputData").value);
-            localStorage.setItem('storageArray', JSON.stringify(lastSearches));
-        }
-
-        function bindEnterKey() {
-            document.querySelector(".inputData").addEventListener('keydown', function (enter) {
-                if (enter.key === "Enter") {
-                    inputSearch();
-                    document.querySelector(".inputSearchList").style.display = "none";
-                }
-            });
-        };
-        bindEnterKey();
-
-        function changeColorOfSearchesList() {
-            let listOfSearches = document.getElementsByTagName('div');
-            let arrayOfSearches = Array.from(listOfSearches);
-            for (i = 0; i < arrayOfSearches.length; i++) {
-                arrayOfSearches[i].onmouseover = arrayOfSearches[i].onmouseout = handler;
+                previousSearchContainer.addEventListener('click', function (event) {
+                    if (!event.target.classList.contains('delete-search')) {
+                        document.querySelector(".inputData").value = searchText.textContent;
+                        inputDiv.style.display = "none";
+                    }
+                })
+                let deleteSearch = previousSearchContainer.appendChild(document.createElement('div'));
+                deleteSearch.className = "delete-search";
+                deleteSearch.textContent = 'x';
+                previousSearchContainer.onmouseover = previousSearchContainer.onmouseout = handler;
                 function handler(event) {
                     if (event.type == "mouseover") {
-                        event.target.style.background = "grey";
-                        document.querySelector(".cross").style.display = "block";
+                        deleteSearch.style.display = "block";
                     }
                     if (event.type == "mouseout") {
-                        event.target.style.background = "white";
+                        deleteSearch.style.display = "none";
                     }
                 }
-            }
+                deleteSearch.addEventListener('click', function () {
+                    if (document.querySelector(".inputData").value === searchText.textContent) {
+                        document.querySelector(".inputData").value = "";
+                    }
+                    previousSearchContainer.remove();
+                    lastSearches.splice(i, 1);
+                    localStorage.setItem('storageArray', JSON.stringify(lastSearches));
+                })
+            };
         }
--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 
-function inputSearch() {
-  let inputData = document.querySelector(".inputData").value;
-  findWeatherData(inputData);
+const apikey = "7e4d1602c64a4d064ef336db6d4bdc24";
+
+window.onload = async function findWeatherInCurrentLocation() {
+  let latitude;
+  let longitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(async function (position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=metric`);
+      let data = await response.json();
+      showWeather(data)
+    });
+  }
+}
+
+function getLastStoredSearches() {
   let stored = localStorage.getItem('storageArray');
   let lastSearches;
   if (stored === null) {
-    lastSearches = []
+    lastSearches = [];
   } else {
-    lastSearches = JSON.parse(stored)
+    lastSearches = JSON.parse(stored);
   }
+  return lastSearches
+}
+
+async function inputSearch() {
+  let inputData = document.querySelector(".inputData").value;
+  showWeather(await findWeatherData(inputData));
+  lastSearches = getLastStoredSearches();
   if (lastSearches.length >= 10) {
     lastSearches.shift();
   }
-  lastSearches.push(document.querySelector(".inputData").value);
+  lastSearches.unshift(inputData);
   localStorage.setItem('storageArray', JSON.stringify(lastSearches));
-  console.log(localStorage.getItem('storageArray'))
 }
+
 
 function bindEnterKey() {
   document.querySelector(".inputData").addEventListener('keydown', function (enter) {
@@ -26,13 +48,13 @@ function bindEnterKey() {
 };
 bindEnterKey();
 
-//let todayWeatherIcon = document.querySelector("#todayIcon");
-
 async function findWeatherData(city) {
   let apikey = "7e4d1602c64a4d064ef336db6d4bdc24"
   let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=metric`);
-  let data = await response.json();
-  console.log(data);
+  return await response.json();
+}
+
+function showWeather(data) {
   document.querySelector(".todayTemperature").innerText = Math.round(data.list[0].main.temp) + "\xB0";
   document.querySelector("#three").innerText = data.list[0].main.humidity + '%';
   document.querySelector("#max-temp-data").innerText = Math.round(data.list[0].main.temp_max) + " \xB0" + "C";
@@ -42,7 +64,6 @@ async function findWeatherData(city) {
   document.querySelector("#nine").innerText = Math.round(data.list[0].wind.speed) + " m/s";
   let weatherIconCode = data.list[0].weather[0].icon;
   document.querySelector("#weather-icon").src = `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`
-
 
   let unixTimeSunset = data.city.sunset;
   let date = new Date(unixTimeSunset * 1000);
@@ -60,47 +81,16 @@ async function findWeatherData(city) {
   document.querySelector("#twelve").innerText = sunriseFormattedTime;
 }
 
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let d = new Date();
-console.log(d);
-let dayOfWeek = weekday[d.getDay()];
-console.log(dayOfWeek);
-let todayDate = d.getDate();
-console.log(todayDate);
-let todayMonth = months[d.getMonth()];
-console.log(todayMonth);
-let year = d.getFullYear();
-console.log(year);
-document.querySelector("#today-day").innerText = `${todayMonth} ${todayDate}`;
 
-let humidityData = document.querySelector("#three").innerText;
-let sunsetTime = document.querySelector("#six").innerText;
-let sunriseTime = document.querySelector("#twelve").innerText;
-
-/*for (let i = 0; i < 5; i++) {
-  let nextDay = new Date();
-  nextDay.setDate(nextDay.getDate() + i)
-  //console.log(nextDay)
+function setDates() {
   const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let dayOfWeek = weekday[nextDay.getDay()];
-  //console.log(dayOfWeek)
-  let todayDate = nextDay.getDate() - 1;
-  //console.log(todayDate);
-  let todayMonth = months[nextDay.getMonth()];
-  //console.log(todayMonth);
-  //console.log(`${dayOfWeek}, ${todayMonth} ${todayDate}`)
-  let dayForecast = document.getElementsByClassName("day-forecast")[i];
-  //console.log(dayForecast);
-  let weekDayElement = dayForecast.getElementsByClassName("week-day")[0];
-  //console.log(weekDayElement)
-  weekDayElement.textContent = "`${dayOfWeek}, ${todayMonth} ${todayDate}`"
-}*/
-
-function setForecastDates() {
-  const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let d = new Date();
+  let dayOfWeek = weekday[d.getDay()];
+  let todayDate = d.getDate();
+  let todayMonth = months[d.getMonth()];
+  let year = d.getFullYear();
+  document.querySelector("#today-day").innerText = `${todayMonth} ${todayDate}`;
   for (let i = 0; i < 5; i++) {
     let nextDay = new Date();
     nextDay.setDate(nextDay.getDate() + i)
@@ -112,7 +102,10 @@ function setForecastDates() {
     weekDayElement.textContent = `${dayOfWeek} ${todayMonth} ${todayDate}`;
   }
 }
-setForecastDates()
+setDates()
+
+
+
 
 
 
