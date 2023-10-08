16ceb22124f59a37041c250c9dafcbda8c4549d7
VeronikaKhenskaya <khenskaya.veranika@gmail.com>
2023-10-08 14:16:07 +0300

    inputSearch function changes

--- input-search.js
+++ input-search.js
@@ -1,60 +1,61 @@
- document.addEventListener('click', function hideLastSearches(event) {
-            const inputDiv = document.querySelector(".inputSearchList");
-            const inputField = document.querySelector(".inputData");
-            if (!inputDiv.contains(event.target) && !inputField.contains(event.target)) {
-                inputDiv.replaceChildren();
+//перезаписывает данные поиска при клике на странице
+document.addEventListener('click', function hideLastSearches(event) {
+    const inputDiv = document.querySelector(".inputSearchList");
+    const inputField = document.querySelector(".inputData");
+    if (!inputDiv.contains(event.target) && !inputField.contains(event.target)) {
+        inputDiv.replaceChildren();
+        inputDiv.style.display = "none";
+    }
+})
+//создает элементы поиска, удаляет элемент из поиска, отображает элементы поиска
+function showLastSearches() {
+    let inputDiv = document.querySelector(".inputSearchList");
+    let lastSearches = getLastStoredSearches();
+    if (lastSearches.length === 0) {
+        inputDiv.style.display = "none";
+    } else {
+        inputDiv.style.display = "block";
+    }
+    let list = document.createElement('div');
+    list.className = "list-container";
+    inputDiv.replaceChildren(list);
+    for (let i = 0; i < lastSearches.length; i++) {
+        let previousSearchContainer = list.appendChild(document.createElement('div'));
+        previousSearchContainer.className = "previous-search-container";
+        let searchText = previousSearchContainer.appendChild(document.createElement('div'));
+        searchText.className = "search-text";
+        searchText.textContent = `${lastSearches[i]}`;
+        searchText.addEventListener('click', function (event) {
+            if (!event.target.classList.contains('delete-search')) {
+                document.querySelector(".inputData").value = event.target.textContent;
                 inputDiv.style.display = "none";
             }
         })
-
-        function showLastSearches() {
-            let inputDiv = document.querySelector(".inputSearchList");
-            let lastSearches = getLastStoredSearches();
-            if (lastSearches.length === 0) {
+        previousSearchContainer.addEventListener('click', function (event) {
+            if (!event.target.classList.contains('delete-search')) {
+                document.querySelector(".inputData").value = searchText.textContent;
                 inputDiv.style.display = "none";
-            } else {
-                inputDiv.style.display = "block";
             }
-            let list = document.createElement('div');
-            list.className = "list-container";
-            inputDiv.replaceChildren(list);
-            for (let i = 0; i < lastSearches.length; i++) {
-                let previousSearchContainer = list.appendChild(document.createElement('div'));
-                previousSearchContainer.className = "previous-search-container";
-                let searchText = previousSearchContainer.appendChild(document.createElement('div'));
-                searchText.className = "search-text";
-                searchText.textContent = `${lastSearches[i]}`;
-                searchText.addEventListener('click', function (event) {
-                    if (!event.target.classList.contains('delete-search')) {
-                        document.querySelector(".inputD ata").value = event.target.textContent;
-                        inputDiv.style.display = "none";
-                    }
-                })
-                previousSearchContainer.addEventListener('click', function (event) {
-                    if (!event.target.classList.contains('delete-search')) {
-                        document.querySelector(".inputData").value = searchText.textContent;
-                        inputDiv.style.display = "none";
-                    }
-                })
-                let deleteSearch = previousSearchContainer.appendChild(document.createElement('div'));
-                deleteSearch.className = "delete-search";
-                deleteSearch.textContent = 'x';
-                previousSearchContainer.onmouseover = previousSearchContainer.onmouseout = handler;
-                function handler(event) {
-                    if (event.type == "mouseover") {
-                        deleteSearch.style.display = "block";
-                    }
-                    if (event.type == "mouseout") {
-                        deleteSearch.style.display = "none";
-                    }
-                }
-                deleteSearch.addEventListener('click', function () {
-                    if (document.querySelector(".inputData").value === searchText.textContent) {
-                        document.querySelector(".inputData").value = "";
-                    }
-                    previousSearchContainer.remove();
-                    lastSearches.splice(i, 1);
-                    localStorage.setItem('storageArray', JSON.stringify(lastSearches));
-                })
-            };
+        })
+        let deleteSearch = previousSearchContainer.appendChild(document.createElement('div'));
+        deleteSearch.className = "delete-search";
+        deleteSearch.textContent = 'x';
+        previousSearchContainer.onmouseover = previousSearchContainer.onmouseout = handler;
+        function handler(event) {
+            if (event.type == "mouseover") {
+                deleteSearch.style.display = "block";
+            }
+            if (event.type == "mouseout") {
+                deleteSearch.style.display = "none";
+            }
         }
+        deleteSearch.addEventListener('click', function () {
+            if (document.querySelector(".inputData").value === searchText.textContent) {
+                document.querySelector(".inputData").value = "";
+            }
+            previousSearchContainer.remove();
+            lastSearches.splice(i, 1);
+            localStorage.setItem('storageArray', JSON.stringify(lastSearches));
+        })
+    };
+}
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 const apikey = "7e4d1602c64a4d064ef336db6d4bdc24";
-
+//находит текущее местоположение и загружает погодные данные текущего местоположения
 window.onload = async function findWeatherInCurrentLocation() {
   let latitude;
   let longitude;
@@ -16,72 +15,7 @@ window.onload = async function findWeatherInCurrentLocation() {
   }
 }
 
-function getLastStoredSearches() {
-  let stored = localStorage.getItem('storageArray');
-  let lastSearches;
-  if (stored === null) {
-    lastSearches = [];
-  } else {
-    lastSearches = JSON.parse(stored);
-  }
-  return lastSearches
-}
-
-async function inputSearch() {
-  let inputData = document.querySelector(".inputData").value;
-  showWeather(await findWeatherData(inputData));
-  lastSearches = getLastStoredSearches();
-  if (lastSearches.length >= 10) {
-    lastSearches.shift();
-  }
-  lastSearches.unshift(inputData);
-  localStorage.setItem('storageArray', JSON.stringify(lastSearches));
-}
-
-
-function bindEnterKey() {
-  document.querySelector(".inputData").addEventListener('keydown', function (enter) {
-    if (enter.key === "Enter") {
-      inputSearch();
-    }
-  });
-};
-bindEnterKey();
-
-async function findWeatherData(city) {
-  let apikey = "7e4d1602c64a4d064ef336db6d4bdc24"
-  let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=metric`);
-  return await response.json();
-}
-
-function showWeather(data) {
-  document.querySelector(".todayTemperature").innerText = Math.round(data.list[0].main.temp) + "\xB0";
-  document.querySelector("#three").innerText = data.list[0].main.humidity + '%';
-  document.querySelector("#max-temp-data").innerText = Math.round(data.list[0].main.temp_max) + " \xB0" + "C";
-  document.querySelector("#min-temp-data").innerText = Math.round(data.list[0].main.temp_min) + " \xB0" + "C";
-  document.querySelector(".location").innerText = `${data.city.name}, ${data.city.country}`;
-  document.querySelector("#weather-description").innerText = data.list[0].weather[0].description;
-  document.querySelector("#nine").innerText = Math.round(data.list[0].wind.speed) + " m/s";
-  let weatherIconCode = data.list[0].weather[0].icon;
-  document.querySelector("#weather-icon").src = `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`
-
-  let unixTimeSunset = data.city.sunset;
-  let date = new Date(unixTimeSunset * 1000);
-  let sunsetHours = date.getHours();
-  let sunsetMinutes = date.getMinutes();
-  let formattedTime = sunsetHours + ':' + sunsetMinutes;
-  document.querySelector(".sunset-time").innerText = `${formattedTime} \n Sunset time`;
-  document.querySelector("#six").innerText = formattedTime;
-
-  let unixTimeSunrise = data.city.sunrise;
-  let date1 = new Date(unixTimeSunrise * 1000);
-  let sunriseHours = date1.getHours();
-  let sunriseMinutes = date1.getMinutes();
-  let sunriseFormattedTime = sunriseHours + ':' + sunriseMinutes;
-  document.querySelector("#twelve").innerText = sunriseFormattedTime;
-}
-
-
+//вычисляет и отображает текущую дату, месяц и следующие пять дней
 function setDates() {
   const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -103,6 +37,86 @@ function setDates() {
   }
 }
 setDates()
+
+//показывает найденные погодные данные
+function showWeather(data) {
+  document.querySelector(".todayTemperature").innerText = Math.round(data.list[0].main.temp) + "\xB0";
+  document.querySelector("#three").innerText = data.list[0].main.humidity + '%';
+  document.querySelector("#max-temp-data").innerText = Math.round(data.list[0].main.temp_max) + " \xB0" + "C";
+  document.querySelector("#min-temp-data").innerText = Math.round(data.list[0].main.temp_min) + " \xB0" + "C";
+  document.querySelector(".location").innerText = `${data.city.name}, ${data.city.country}`;
+  document.querySelector("#weather-description").innerText = data.list[0].weather[0].description;
+  document.querySelector("#nine").innerText = Math.round(data.list[0].wind.speed) + " m/s";
+  let weatherIconCode = data.list[0].weather[0].icon;
+  document.querySelector("#weather-icon").src = `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`;
+  document.querySelector(".inputData").value = `${data.city.name}`;
+  // вычисляет и показывает время заката
+  let unixTimeSunset = data.city.sunset;
+  let date = new Date(unixTimeSunset * 1000);
+  let sunsetHours = date.getHours();
+  let sunsetMinutes = date.getMinutes();
+  let formattedTime = sunsetHours + ':' + sunsetMinutes;
+  document.querySelector(".sunset-time").innerText = `${formattedTime} \n Sunset time`;
+  document.querySelector("#six").innerText = formattedTime;
+  // вычисляет и показывает время рассвета
+  let unixTimeSunrise = data.city.sunrise;
+  let date1 = new Date(unixTimeSunrise * 1000);
+  let sunriseHours = date1.getHours();
+  let sunriseMinutes = date1.getMinutes();
+  let sunriseFormattedTime = sunriseHours + ':' + sunriseMinutes;
+  document.querySelector("#twelve").innerText = sunriseFormattedTime;
+}
+
+//вызывает функции поиска погодных данных по введенному городу в input и сохраняет данные поиска в localStorage
+async function inputSearch() {
+  let inputData = document.querySelector(".inputData").value;
+  showWeather(await findWeatherData(inputData));
+  lastSearches = getLastStoredSearches();
+  if (lastSearches.length >= 10) {
+    lastSearches.shift();
+  }
+  if (!lastSearches.includes(inputData)) {
+    lastSearches.unshift(inputData);
+    localStorage.setItem('storageArray', JSON.stringify(lastSearches));
+  }
+  for (i = 0; i < lastSearches.length; i++) {
+    if (lastSearches[i] === inputData) {
+      lastSearches.splice(i, 1);
+      lastSearches.unshift(inputData);
+      localStorage.setItem('storageArray', JSON.stringify(lastSearches));
+    }
+  }
+}
+
+//вызывает функцию inputSearch по нажатию на "Enter"
+function bindEnterKey() {
+  document.querySelector(".inputData").addEventListener('keydown', function (enter) {
+    if (enter.key === "Enter") {
+      inputSearch();
+    }
+  });
+};
+bindEnterKey();
+
+//загружает данные погоды с помощью api
+async function findWeatherData(city) {
+  let apikey = "7e4d1602c64a4d064ef336db6d4bdc24"
+  let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=metric`);
+  return await response.json();
+}
+
+//возвращает массив данных поиска из localStorage
+function getLastStoredSearches() {
+  let stored = localStorage.getItem('storageArray');
+  let lastSearches;
+  if (stored === null) {
+    lastSearches = [];
+  } else {
+    lastSearches = JSON.parse(stored);
+  }
+  return lastSearches
+}
+
 
 
 
